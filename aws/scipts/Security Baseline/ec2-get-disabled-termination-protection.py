cb76443f5930f86167c41ff2723fb3ae6ca155a3
Stephen Morgan <smorgan@privoit.com>
2021-05-01 10:58:11 -0700

    added readme

--- aws/scipts/Security Baseline/ec2-get-disabled-termination-protection.py
+++ aws/scipts/Security Baseline/ec2-get-disabled-termination-protection.py
@@ -1,14 +1,14 @@
 import boto3
 import json
 
-# This script gets the running instances with termination protection 
+# This script gets the running instances with termination protection
 # disabled.
 
 # Set resource
 ec2 = boto3.resource('ec2')
 client = boto3.client('ec2')
 
-# Get instances using filters for running 
+# Get instances using filters for running
 instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
@@ -18,9 +18,9 @@ for instance in instances:
     Attribute='disableApiTermination',
     InstanceId=instance.id,
     )
-    
+
     for key in response:
         if key == 'DisableApiTermination':
             if (response[key]['Value'] == False):
-                print(json.dumps(response, sort_keys=True, indent=4))
+                print(json.dumps(response, sort_keys=True, indent=4, default=str))
 
